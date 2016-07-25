8b765077fd9e3a457f48ecb6e7088d881cb14503
iwandi <ackermanna@gmx.de>
2016-07-26 00:40:30 +0200

    Added InitCommandBuffer Example
    TODO : init layers and Extentions in SampleInstance

---
+++ Vulkan.SharpLang.Examples/InitCommandBuffer/SampleInitCommandBuffer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using Vulkan;
+
+
+namespace Vulkan.SharpLang.Examples
+{
+    class SampleInitCommandBuffer
+    {
+        static void Main(string[] args)
+        {
+            SampleInstance sample = new SampleInstance();
+            Instance instance = sample.InitInstance("Command Buffer Sample");
+
+            sample.InitEnumerateDevice();
+            Device device = sample.InitDevice();
+            sample.QueueFamilyIndex();
+
+            CommandPoolCreateInfo cmdPoolInfo = new CommandPoolCreateInfo
+            {
+                QueueFamilyIndex = sample.GraphicsQueueFamilyIndex,
+                Flags = 0,
+            };
+
+            CommandPool cmdPool = device.CreateCommandPool(cmdPoolInfo);
+
+            CommandBufferAllocateInfo cmd = new CommandBufferAllocateInfo
+            {
+                CommandPool = cmdPool,
+                Level = CommandBufferLevel.Primary,
+                CommandBufferCount = 1,
+            };
+
+            CommandBuffer[] cmdBufs = device.AllocateCommandBuffers(cmd);
+
+            device.FreeCommandBuffers(cmdPool, cmdBufs);
+            device.DestroyCommandPool(cmdPool);
+            device.Destroy();
+            instance.Destroy();
+        }
+    }
+}
--- Vulkan.SharpLang.Examples/SamplesPlatform/SampleInstance.cs
+++ Vulkan.SharpLang.Examples/SamplesPlatform/SampleInstance.cs
@@ -33,10 +33,41 @@ namespace Vulkan.SharpLang.Examples
         PhysicalDevice[] gpus;
         public PhysicalDevice[] Gpus {  get {  return gpus; } }
 
+        uint graphicsQueueFamilyIndex; // TODO : Set this
+        public uint GraphicsQueueFamilyIndex {  get { return graphicsQueueFamilyIndex; } }
+
         public PhysicalDevice[] InitEnumerateDevice()
         {
-            PhysicalDevice[] gpus = instance.EnumeratePhysicalDevices();
+            gpus = instance.EnumeratePhysicalDevices();
             return gpus;
+        }
+
+        Device device;
+        public Device Device {  get { return device; } }
+
+        public Device InitDevice()
+        {
+            DeviceQueueCreateInfo queueInfo = new DeviceQueueCreateInfo
+            {
+                QueueCount = 1,
+                QueueFamilyIndex = graphicsQueueFamilyIndex,
+                QueuePriorities = new float[] { 0.0f },
+            };
+
+            DeviceCreateInfo info = new DeviceCreateInfo
+            {
+                QueueCreateInfos = new DeviceQueueCreateInfo[] { queueInfo },
+                EnabledLayerNames = new string[0], // TODO Enabled Layers
+                EnabledExtensionNames = new string[0], // TODO Enabled Extentions
+            };
+
+            device = gpus[0].CreateDevice(info);
+            return device;
+        }
+
+        public void QueueFamilyIndex()
+        {
+            // TODO 
         }
     }
 }
